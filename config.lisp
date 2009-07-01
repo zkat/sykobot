7257a547bd1193b028973dc96e354683f086ecff
Kat Marchán <kzm@sykosomatic.org>
2009-07-01 12:25:28 -0500

    tweaking scripting stuff

--- config.lisp
+++ config.lisp
@@ -1,15 +1,30 @@
 (in-package :sykobot)
 
+(defvar *default-channels* nil)
+(defvar *server* nil)
+(defvar *identify-with-nickserv?* nil)
+(defvar *nickserv-password* nil)
+
 (defun config-exists-p ()
   (if (probe-file (merge-pathnames ".sykobotrc" (user-homedir-pathname)))
       t nil))
 
-(defun run-sykobot (&optional (server "irc.freenode.net"))
+(defun load-rc-file ()
+  (load (merge-pathnames ".sykobotrc" (user-homedir-pathname))))
+
+(defun run-sykobot ()
   (handler-bind ((cl-irc:no-such-reply (lambda (c)
                                          (let ((r (find-restart 'continue c)))
                                            (when r (invoke-restart r))))))
-    (if (config-exists-p)
-        (load (merge-pathnames ".sykobotrc" (user-homedir-pathname)))
-        (progn (setf (server #@sykobot) server)
-               (run-bot #@sykobot)))))
+    (when (config-exists-p)
+      (load (merge-pathnames ".sykobotrc" (user-homedir-pathname))))
+    (when *server
+      (setf (server #@sykobot) *server*))
+    (run-bot #@sykobot)
+    (when *identify-with-nickserv?*
+      (identify #@sykobot *nickserv-password*))
+    (loop for channel in *default-channels*
+       do (join #@sykobot channel))))
+
+
 
