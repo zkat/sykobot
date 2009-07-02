52775f76e9b9b45c93e9a7344306b52f0f307ff5
Adlai <munchking@gmail.com>
2009-07-02 17:16:40 +0300

    Starting Reorganization
    Created separate file for chanting.
    Minor bugfix in commands.
    
    Commits will be made frequently, to avoid the stupidity that happened
    earlier today -- I went through the entire chant system, commented the
    parts, and improved it a bit, then lost the changes by being a gitiot.

--- commands.lisp
+++ commands.lisp
@@ -28,7 +28,6 @@
       (think bot channel))
   (sleep (1+ (random max-time))))
 
-(defvar *more* "lulz")
 (add-command "think" (lambda (bot args sender channel)
 		       (think bot channel 2)))
 (add-command "echo" (lambda (bot args sender channel)
@@ -43,11 +42,9 @@
                              (format nil "~A <~A>" title url)))))
 (add-command "shut" (lambda (bot args sender channel)
                       (send-reply bot sender channel
-                                  "Fine. Be that way. Tell me to talk when you realize ~
-                                   just how lonely and pathetic you really are.")
+                                  (format nil "Fine. Be that way. Tell me to talk when you realize ~
+                                               just how lonely and pathetic you really are."))
                       (shut-up bot)))
-(add-command "chant" (lambda (bot args sender channel)
-                      (send-msg bot channel (format nil "MORE ~:@(~A~)" *more*))))
 (add-command "help" (lambda (bot args sender channel)
                       (send-reply bot sender channel "I'm not a psychiatrist. Go away.")))
 #+nil(add-command "tell" (lambda (bot args sender channel)
@@ -75,47 +72,6 @@
                                           (format nil "tada! Added memo for ~A. ~
                                                        I'll let them know next time they speak"
                                                   recipient))))))
-
-(defvar *prepositions*
-  '("aboard"  "about"  "above"  "across"  "after"  "against"  "along"  "among"  "around"  "as"   "at"
-    "before"  "behind"   "below" "beneath" "beside"  "between"  "beyond"  "but" "except"  "by"  
-    "concerning"  "despite"  "down"  "during"  "except" "for"  "from"  "in"  "into"  "like" "near"
-    "of"  "off"  "on"  "onto"  "out"  "outside"  "over"  "past"  "per"  "regarding"  "since"  "through"
-    "throughout"  "till"  "to"  "toward"  "under" "underneath"  "until"  "up"   "upon"  "with"  
-    "within" "without"))
-(defvar *conjunctions*
-  '("for" "and" "nor" "but" "or" "yet" "so"))
-(defvar *articles*
-  '("an" "a" "the"))
-(defun scan-for-more (s)
-  (let ((str (nth-value 
-              1 (scan-to-strings "[MORE|MOAR]\\W+((\\W|[A-Z0-9])+)([A-Z0-9])($|[^A-Z0-9])" s))))
-    (or
-     (and str
-          (setf *more* (concatenate 'string (elt str 0) (elt str 2))))
-     (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)\\W+(\\w+)" s))))
-       (or
-        (and str
-             (or (member (elt str 0) *prepositions* :test #'string-equal)
-                 (member (elt str 0) *conjunctions* :test #'string-equal)
-                 (member (elt str 0) *articles* :test #'string-equal))
-             (or (member (elt str 1) *prepositions* :test #'string-equal)
-                 (member (elt str 1) *conjunctions* :test #'string-equal)
-                 (member (elt str 1) *articles* :test #'string-equal))
-             (setf *more* (string-upcase
-                           (concatenate 'string (elt str 0) " " (elt str 1)
-                                        " " (elt str 2)))))
-        (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)" s))))
-          (or
-           (and str
-                (or (member (elt str 0) *prepositions* :test #'string-equal)
-                    (member (elt str 0) *conjunctions* :test #'string-equal)
-                    (member (elt str 0) *articles* :test #'string-equal))
-                (setf *more* (string-upcase
-                              (concatenate 'string (elt str 0) " " (elt str 1)))))
-           (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)" s))))
-             (or
-              (and str (setf *more* (string-upcase (elt str 0)))))))))))))
 
 (defun get-ks-time ()
   (multiple-value-bind
