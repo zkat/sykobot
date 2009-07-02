8b1a91676b9d114e49db368db12a53c2bfb19285
Adlai <munchking@gmail.com>
2009-07-02 22:26:00 +0300

    Added some comments

--- chant.lisp
+++ chant.lisp
@@ -1,24 +1,41 @@
+;;; The code in this file is based on the chant routines
+;;;   of 'minon' on #lisp.
 (in-package :sykobot)
 
+;;; These variables assist in parsing messages
 (defvar *prepositions*
   '("aboard"  "about"  "above"  "across"  "after"  "against"  "along"  "among"  "around"  "as"   "at"
-    "before"  "behind"   "below" "beneath" "beside"  "between"  "beyond"  "but" "except"  "by"  
+    "before"  "behind"   "below" "beneath" "beside"  "between"  "beyond"  "but" "except"  "by"
     "concerning"  "despite"  "down"  "during"  "except" "for"  "from"  "in"  "into"  "like" "near"
     "of"  "off"  "on"  "onto"  "out"  "outside"  "over"  "past"  "per"  "regarding"  "since"  "through"
-    "throughout"  "till"  "to"  "toward"  "under" "underneath"  "until"  "up"   "upon"  "with"  
+    "throughout"  "till"  "to"  "toward"  "under" "underneath"  "until"  "up"   "upon"  "with"
     "within" "without"))
 (defvar *conjunctions*
   '("for" "and" "nor" "but" "or" "yet" "so"))
 (defvar *articles*
   '("an" "a" "the"))
 
+;;; This variable stores the current 'fad'.
 (defvar *more* "lulz")
 
+;;; This command is used to chant.
 (add-command "chant" (lambda (bot args sender channel)
                       (send-msg bot channel (format nil "MORE ~:@(~A~)" *more*))))
 
+;;; This is where the cookie crumbles.
+;;;
+;;; More detailed documentation coming soon; essentially, each
+;;;   regexp scans an increasingly general search, trying to
+;;;   find the current 'fad'. The boolean functions essentially
+;;;   ensure that once a match is found, the function will exit.
+;;;
+;;; Because the scans are arranged in decreasing specificity, this
+;;;   means that the most specific 'fad' will be cached.
+;;;
+;;; This is buggy, for reasons which I don't have time to go into.
+;;; I have a fix and will commit in a few hours.
 (defun scan-for-more (s)
-  (let ((str (nth-value 
+  (let ((str (nth-value
               1 (scan-to-strings "[MORE|MOAR]\\W+((\\W|[A-Z0-9])+)([A-Z0-9])($|[^A-Z0-9])" s))))
     (or
      (and str
