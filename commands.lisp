7b81039b30628dff072cb57dacdc1a72c5739de9
Kat Marchán <kzm@sykosomatic.org>
2009-07-01 19:51:21 -0500

    MORE COMMANDS

--- commands.lisp
+++ commands.lisp
@@ -18,15 +18,17 @@
   (defun erase-all-commands ()
     (clrhash command-table)))
 
-(defun think (bot channel)
-  (send-action bot channel "thinks"))
+(defun think (bot channel &optional (minimum-delay 0))
+  (send-action bot channel "thinks")
+  (+ minimum-delay (sleep 8))
+  (send-msg bot channel "Hmm.... Indeed."))
 
 (defun pause-in-thought (bot channel &key (max-time 5) (action-probability 2))
   (if (zerop (random action-probability))
       (think bot channel))
   (sleep (1+ (random max-time))))
 
-
+(defvar *more* "lulz")
 (add-command "think" (lambda (bot args sender channel)
 		       (think bot channel 2)))
 
@@ -38,21 +40,21 @@
              (lambda (bot args sender channel)
                (multiple-value-bind (title url)
                    (google-search args)
-                 (send-msg bot channel
-                           (format nil "~A: ~A <~A>"
-                                   sender title url)))))
+                 (send-reply bot sender channel
+                             (format nil "~A <~A>" title url)))))
 (add-command "shut" (lambda (bot args sender channel)
-                      (send-msg bot channel
-                                (format nil "~A: Fine. Be that way. Tell me to talk when you realize ~
-                                                 just how lonely and pathetic you really are." sender))
+                      (send-reply bot sender channel
+                                  "Fine. Be that way. Tell me to talk when you realize ~
+                                   just how lonely and pathetic you really are.")
                       (shut-up bot)))
 (add-command "chant" (lambda (bot args sender channel)
-                      (send-msg bot channel "FUCK REGEX")))
+                      (send-msg bot channel (format nil "MORE ~:@(~A~)" *more*))))
 (add-command "help" (lambda (bot args sender channel)
                       (send-msg bot channel (format nil "~A: I'm not a psychiatrist. Go away." sender))))
 #+nil(add-command "tell" (lambda (bot args sender channel)
                       (send-msg bot channel)))
-
+(add-command "exit" (lambda (bot args sender channel)
+                      (send-reply bot sender channel "1 ON 1 FAGGOT")))
 (add-command "cliki"
              (lambda (bot args sender channel)
                (send-msg bot channel
@@ -60,12 +62,52 @@
                              (cliki-urls args)
                            (format nil "~A, I found ~D result~:P.~@[ Check out <~A>.~]"
                                    sender numlinks (car links))))))
-
 (add-command "kiloseconds"
              (lambda (bot args sender channel)
                (send-msg bot channel
                          (format nil "~A, the time is GMT ~3$ ks."
                                  sender (get-ks-time)))))
+
+(defvar *prepositions*
+  '("aboard"  "about"  "above"  "across"  "after"  "against"  "along"  "among"  "around"  "as"   "at"
+    "before"  "behind"   "below" "beneath" "beside"  "between"  "beyond"  "but" "except"  "by"  
+    "concerning"  "despite"  "down"  "during"  "except" "for"  "from"  "in"  "into"  "like" "near"
+    "of"  "off"  "on"  "onto"  "out"  "outside"  "over"  "past"  "per"  "regarding"  "since"  "through"
+    "throughout"  "till"  "to"  "toward"  "under" "underneath"  "until"  "up"   "upon"  "with"  
+    "within" "without"))
+(defvar *conjunctions*
+  '("for" "and" "nor" "but" "or" "yet" "so"))
+(defvar *articles*
+  '("an" "a" "the"))
+(defun scan-for-more (s)
+  (let ((str (nth-value 
+              1 (scan-to-strings "[MORE|MOAR]\\W+((\\W|[A-Z0-9])+)([A-Z0-9])($|[^A-Z0-9])" s))))
+    (or
+     (and str
+          (setf *more* (concatenate 'string (elt str 0) (elt str 2))))
+     (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)\\W+(\\w+)" s))))
+       (or
+        (and str
+             (or (member (elt str 0) *prepositions* :test #'string-equal)
+                 (member (elt str 0) *conjunctions* :test #'string-equal)
+                 (member (elt str 0) *articles* :test #'string-equal))
+             (or (member (elt str 1) *prepositions* :test #'string-equal)
+                 (member (elt str 1) *conjunctions* :test #'string-equal)
+                 (member (elt str 1) *articles* :test #'string-equal))
+             (setf *more* (string-upcase
+                           (concatenate 'string (elt str 0) " " (elt str 1)
+                                        " " (elt str 2)))))
+        (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)" s))))
+          (or
+           (and str
+                (or (member (elt str 0) *prepositions* :test #'string-equal)
+                    (member (elt str 0) *conjunctions* :test #'string-equal)
+                    (member (elt str 0) *articles* :test #'string-equal))
+                (setf *more* (string-upcase
+                              (concatenate 'string (elt str 0) " " (elt str 1)))))
+           (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)" s))))
+             (or
+              (and str (setf *more* (string-upcase (elt str 0)))))))))))))
 
 (defun get-ks-time ()
   (multiple-value-bind
