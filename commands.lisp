643bf93c6ce46b20c59765fe1de730d9ebd0e19b
Ben Duffield <jebavarde@gmail.com>
2009-07-04 22:10:48 +0100

    Refactored scan-for-url and scan-for-more to use listeners

--- commands.lisp
+++ commands.lisp
@@ -161,35 +161,40 @@
   '("for" "and" "nor" "but" "or" "yet" "so"))
 (defvar *articles*
   '("an" "a" "the"))
-(defun scan-for-more (s)
-  (let ((str (nth-value
-              1 (scan-to-strings "[MORE|MOAR]\\W+((\\W|[A-Z0-9])+)([A-Z0-9])($|[^A-Z0-9])" s))))
-    (or
-     (and str
-          (setf *more* (concatenate 'string (elt str 0) (elt str 2))))
-     (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)\\W+(\\w+)" s))))
-       (or
-        (and str
-             (or (member (elt str 0) *prepositions* :test #'string-equal)
-                 (member (elt str 0) *conjunctions* :test #'string-equal)
-                 (member (elt str 0) *articles* :test #'string-equal))
-             (or (member (elt str 1) *prepositions* :test #'string-equal)
-                 (member (elt str 1) *conjunctions* :test #'string-equal)
-                 (member (elt str 1) *articles* :test #'string-equal))
-             (setf *more* (string-upcase
-                           (concatenate 'string (elt str 0) " " (elt str 1)
-                                        " " (elt str 2)))))
-        (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)" s))))
-          (or
-           (and str
-                (or (member (elt str 0) *prepositions* :test #'string-equal)
-                    (member (elt str 0) *conjunctions* :test #'string-equal)
-                    (member (elt str 0) *articles* :test #'string-equal))
-                (setf *more* (string-upcase
-                              (concatenate 'string (elt str 0) " " (elt str 1)))))
-           (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)" s))))
-             (or
-              (and str (setf *more* (string-upcase (elt str 0)))))))))))))
+
+
+(deflistener "scan-for-more"
+  (let ((s message))
+    (let ((str (nth-value
+		1 (scan-to-strings "[MORE|MOAR]\\W+((\\W|[A-Z0-9])+)([A-Z0-9])($|[^A-Z0-9])" s))))
+      (or
+       (and str
+	    (setf *more* (concatenate 'string (elt str 0) (elt str 2))))
+       (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)\\W+(\\w+)" s))))
+	 (or
+	  (and str
+	       (or (member (elt str 0) *prepositions* :test #'string-equal)
+		   (member (elt str 0) *conjunctions* :test #'string-equal)
+		   (member (elt str 0) *articles* :test #'string-equal))
+	       (or (member (elt str 1) *prepositions* :test #'string-equal)
+		   (member (elt str 1) *conjunctions* :test #'string-equal)
+		   (member (elt str 1) *articles* :test #'string-equal))
+	       (setf *more* (string-upcase
+			     (concatenate 'string (elt str 0) " " (elt str 1)
+					  " " (elt str 2)))))
+	  (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)\\W+(\\w+)" s))))
+	    (or
+	     (and str
+		  (or (member (elt str 0) *prepositions* :test #'string-equal)
+		      (member (elt str 0) *conjunctions* :test #'string-equal)
+		      (member (elt str 0) *articles* :test #'string-equal))
+		  (setf *more* (string-upcase
+				(concatenate 'string (elt str 0) " " (elt str 1)))))
+	     (let ((str (nth-value 1 (scan-to-strings "(?i)[more|moar]\\W+(\\w+)" s))))
+	       (or
+		(and str (setf *more* (string-upcase (elt str 0))))))))))))))
+(activate-listener "scan-for-more")
+
 
 ;;; kiloseconds
 (defcommand "kiloseconds"
