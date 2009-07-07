53f456a11364a99d05c7bcd42e480f3cd984d258
Ben Duffield <jebavarde@gmail.com>
2009-07-07 11:45:41 +0100

    Quotegrabs - Don't fail so much with dodgy arguments.

--- src/utils/utils.lisp
+++ src/utils/utils.lisp
@@ -3,4 +3,8 @@
 ;; general-purpose util functions/macros/etc go here
 
 (defun hash-table-keys (table) (loop for key being the hash-keys of table collect key))
-(defun random-elt (sequence) (elt sequence (random (length sequence))))
+(defun random-elt (sequence) 
+  (let ((l (length sequence)))
+    (if (zerop l)
+	nil
+	(elt sequence (random (length sequence))))))
