620340d14ea76121fc36789c6c1bc3c52174b892
Adlai <munchking@gmail.com>
2009-07-07 14:44:09 +0300

    Added a util

--- src/utils/utils.lisp
+++ src/utils/utils.lisp
@@ -2,5 +2,14 @@
 
 ;; general-purpose util functions/macros/etc go here
 
-(defun hash-table-keys (table) (loop for key being the hash-keys of table collect key))
-(defun random-elt (sequence) (elt sequence (random (length sequence))))
+(defun hash-table-keys (table)
+  (loop for key being the hash-keys of table collect key))
+
+(defun random-elt (sequence)
+  (elt sequence (random (length sequence))))
+
+;;; NOTE: Don't use '\' in the separator, it fails.
+(defun merge-strings (separator &rest strings)
+  (format nil (format nil "~~{~~A~~^~A~~}"
+                      (regex-replace-all "~" separator "~~"))
+          strings))
