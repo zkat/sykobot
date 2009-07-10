20c8c22bb93d5361674ac4b46545606e8b62bd00
Adlai <munchking@gmail.com>
2009-07-10 05:32:56 +0300

    A bit of modularization

--- src/listeners.lisp
+++ src/listeners.lisp
@@ -7,59 +7,64 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :sykobot)
 
-(defmessage activate-listener (bot name))
-(defmessage deactivate-listener (bot name))
-(defmessage call-all-listeners (bot sender channel message))
+;;; These are only bound within the body of listeners.
+(defvar *bot*)
+(defvar *message*)
+(defvar *sender*)
+(defvar *channel*)
+
+;;; Attempt at modularization
+(defproto sykobot-listener ((proto 'sykobot))
+  ((listeners (make-hash-table :test #'eq))
+   (active-listeners nil)))
+
+(defmessage add-listener (bot name function))
+(defmessage remove-listener (bot name))
+(defmessage listener-function (bot name))
+(defmessage call-listener (bot name sender channel message))
+(defmessage call-listeners (bot sender channel message))
 (defmessage listener-active-p (bot name))
 
-(defreply activate-listener ((bot (proto 'sykobot)) name)
-  (pushnew name (active-listeners bot)))
-(defreply deactivate-listener ((bot (proto 'sykobot)) name)
-  (with-properties (active-listeners) bot
-    (setf active-listeners (delete name active-listeners))))
+(defreply add-listener ((bot (proto 'sykobot-listener)) (name (proto 'symbol)) function)
+  (setf (gethash name (listeners bot)) function))
 
-(defreply call-all-listeners ((bot (proto 'sykobot)) sender channel message)
-  (loop for name in (active-listeners bot)
-     do (call-listener bot name sender channel message)))
+(defreply remove-listener ((bot (proto 'sykobot-listener)) (name (proto 'symbol)))
+  (remhash name (listeners bot)))
 
-(defreply listener-active-p ((bot (proto 'sykobot)) name)
-  (if (member name (active-listeners bot))
-      t nil))
+(defreply listener-function ((bot (proto 'sykobot-listener)) (name (proto 'symbol)))
+  (gethash name (listeners bot)
+           (lambda (bot sender channel message)
+             (declare (ignore bot sender channel message))
+             (cerror "Continue" "Nonexistant listener ~S" name))))
 
-(defun activate-listeners (bot &rest listener-names)
-  (loop for listener in listener-names
-       do (activate-listener bot listener)))
+(defreply call-listener ((bot (proto 'sykobot-listener)) (name (proto 'symbol)) sender channel message)
+  (funcall (listener-function bot name) bot sender channel message))
 
-;;; Listeners
-(let ((listener-table (make-hash-table :test #'eq)))
+(defreply call-listeners ((bot (proto 'sykobot-listener)) sender channel message)
+  (dolist (name (active-listeners bot))
+    (call-listener bot name sender channel message)))
 
-  (defun add-listener (name function)
-    (assert (symbolp name))
-    (setf (gethash name listener-table) function))
-
-  (defun remove-listener (name)
-    (remhash name listener-table))
-
-  (defun listener-function (name)
-    (multiple-value-bind (fn hasp)
-        (gethash name listener-table)
-      (if hasp
-          fn
-          (lambda (bot sender channel message)
-            (declare (ignore bot sender channel message))
-            (print "listener doesn't exist")))))
-
-  (defun call-listener (bot name sender channel message)
-    (funcall (listener-function name) bot sender channel message))
-
-  (defun get-listener-table ()
-    listener-table)
-
-  ) ;end listener table
+;;; Where is this used? -Adlai
+(defreply listener-active-p ((bot (proto 'sykobot-listener)) name)
+  (member name (active-listeners bot)))
 
 (defmacro deflistener (name &body body)
-  `(add-listener ',name
+  `(add-listener (proto 'sykobot-listener) ',name
                 (lambda (*bot* *sender* *channel* *message*)
                   (declare (ignorable *bot* *sender* *channel* *message*))
                   ,@body)))
 
+;;; Customization of listeners
+(defmessage listener-on (bot name))
+(defmessage listener-off (bot name))
+
+(defreply listener-on ((bot (proto 'sykobot-listener)) name)
+  (pushnew name (active-listeners bot)))
+
+(defreply listener-off ((bot (proto 'sykobot-listener)) name)
+  (with-properties (active-listeners) bot
+    (setf active-listeners (delete name active-listeners))))
+
+(defun activate-listeners (bot &rest names)
+  (dolist (name names)
+    (listener-on bot name)))
