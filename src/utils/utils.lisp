c53f3f090bb0a98f7787e3bcba6104ecb32e9a03
Adlai <munchking@gmail.com>
2009-07-12 19:27:27 +0300

    Improved #'build-string

--- src/utils/utils.lisp
+++ src/utils/utils.lisp
@@ -14,23 +14,28 @@
   `(let ,(loop for x in vars collect `(,x (gensym)))
      ,@body))
 
-(defun build-string (&rest strings)
-  (when (and (stringp (car strings))
-	     (string-equal (car strings) "~A"))
+(defun build-string (&rest data)
+  ;; This first form is just to catch idiotic code.
+  ;; It should be removed from "out-of-the-box" sykobot distros.
+  (when (and (stringp (car data))
+             (string-equal (car data) "~A"))
     (error "BUILD-STRING is being called like FORMAT.~@
             Please rewrite this call and join the sensation."))
-  (if (cadr strings)
-      (apply #'format nil strings)
-      (funcall #'format nil "~A"
-	       (or (car strings)
-		   ""))))
+  (cond ((null data) "")
+	((not (stringp (car data)))
+	 (warn "Ignoring datum ~A." (car data))
+	 (apply #'build-string (cdr data)))
+	((unless (scan "~[:@]{0,2}\\n" (car data))
+	   (null (cdr data)))
+	 (format nil "~A" (car data)))
+	(T (apply #'format nil data))))
 
 (defmacro do-lines ((var string &optional result) &body body)
   (with-gensyms (stream)
     `(with-input-from-string (,stream ,string)
        (loop for ,var = (read-line ,stream nil)
-	  while ,var ,@body
-	  finally (return ,result)))))
+          while ,var ,@body
+          finally (return ,result)))))
 
 (defun random-elt (sequence)
   (let ((l (length sequence)))
@@ -44,8 +49,8 @@
 ;;; NOTE: Don't use '\' in the separator, it fails.
 (defun merge-strings (separator &rest strings)
   (build-string (build-string "~~{~~A~~^~A~~}"
-			      (regex-replace-all "~" separator "~~"))
-		strings))
+                              (regex-replace-all "~" separator "~~"))
+                strings))
 
 (defun flatten (x)
   "Flattens a list."
