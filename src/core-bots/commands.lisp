79a417fea1a77730c52803a76dc53c5cc1f961a8
jelly <jellevdwaa@gmail.com>
2009-07-16 15:20:22 +0200

    my nowplaying listener

--- src/core-bots/commands.lisp
+++ src/core-bots/commands.lisp
@@ -496,6 +496,3 @@ I love to singa"
        "there are ~A and the temperature is ~A°C at ~A"
        cloudyness temp station-name)
       (build-string "I couldn't find the weather for ~A" location))))
-
-(deflistener scan-for-now-playing (and (> (length *message*) 3)(STRING-equal (subseq *message* 0 3) "np:") (send-reply *bot* *channel* *sender* (random-elt (list "nice song man" "you know your classics" "wut an noise!" "in your pants" "tool is much better than that!" "that's nothing compared to britney")))))
-
