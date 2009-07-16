e7414e2540113cb82501880fcec77cb378fb2729
jelly <jellevdwaa@gmail.com>
2009-07-16 12:59:50 +0200

    new misc-listener

---
+++ src/misc-listeners.lisp
@@ -0,0 +1,11 @@
+ ;;;; Copyright 2009 Kat Marchan
+ ;;;;;;;
+ ;;;;;;; This file is part of sykobot.
+ ;;;;;;;
+ ;;;;;;; For licensing and warranty information, refer to COPYING
+ ;;;;;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (in-package: sykobot)
+
+  (deflistener scan-for-now-playing)
+
