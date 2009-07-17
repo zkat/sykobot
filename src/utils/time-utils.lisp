4f5e33953dc9885d4e78fbf6d7175800ca5f3ee4
Kat Marchán <kzm@sykosomatic.org>
2009-07-16 23:07:36 -0400

    Initial conversion of time-utils to time objects.

--- src/utils/time-utils.lisp
+++ src/utils/time-utils.lisp
@@ -1,31 +1,35 @@
 (in-package :sykobot)
 
 (defun get-ks-timestamp (&key (zone 0) (time (get-universal-time)))
-  (multiple-value-call #'build-string
-    "~3$ ks in GMT~@D" (get-ks-time :zone zone :time time)))
+  (build-string "~3$ ks in GMT~@D" (get-kiloseconds :zone zone :time time) zone))
 
-(defmacro with-decoded-time (time zone &body body)
-  `(multiple-value-bind
-         (seconds minutes hours date month year day light timezone)
-       (decode-universal-time ,time (- ,zone))
-     (declare (ignorable seconds minutes hours date
-			 month year day light timezone))
-     ,@body))
+(defstruct (time (:constructor %make-time))
+  seconds minutes hours date month year day dstp zone)
+
+(defun decoded-time-object (time &optional (zone 0))
+  (multiple-value-bind (seconds minutes hours date month year day dstp zone)
+      (decode-universal-time time zone)
+    (%make-time :seconds seconds :minutes minutes :hours hours :date date
+		:month month :year year :day day :dstp dstp :zone zone)))
+
+(defun get-time-object ()
+  (decoded-time-object (get-universal-time)))
 
 (defun get-boring-timestamp (&key (zone 0) (time (get-universal-time)))
-  (with-decoded-time time zone
+  (with-slots (hours minutes seconds) (decoded-time-object time zone)
     (build-string "~2D:~2D:~2D in GMT~@D" hours minutes seconds
                   (- (mod (+ 11 zone) 24) 11))))
 
 (defun get-datestamp (&key (zone 0) (time (get-universal-time)))
-  (with-decoded-time time zone
+  (with-slots (day month date year) (decoded-time-object time zone)
     (build-string "~[Mon~;Tue~;Wed~;Thu~;Fri~;Sat~;Sun~], ~
                    ~[Jan~;Feb~;Mar~;Apr~;May~;Jun~;Jul~;Aug~;Sep~;Oct~;Nov~;Dec~] ~
                    ~D~[th~;st~;nd~;rd~:;th~], ~D"
                   day (1+ month) date (mod date 10) year)))
 
-(defun get-ks-time (&key (zone 0) (time (get-universal-time)))
-  (let ((zone (- (mod (+ 11 zone) 24) 11)))
-    (values (with-decoded-time time zone
-              (/ (+ seconds (* 60 (+ minutes (* 60 hours)))) 1000.0))
-            zone)))
+(defun get-kiloseconds (&key (zone 0) (time (get-universal-time)))
+  (with-slots (seconds minutes hours) (decoded-time-object time zone)
+    (/ (+ seconds 
+	  (* minutes 60)
+	  (* hours 3600))
+       1000.0)))
