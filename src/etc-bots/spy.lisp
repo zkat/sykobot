2df1a51a6cadbd031eedec65649701aa7a892fbd
Adlai <munchking@gmail.com>
2009-07-19 00:02:48 +0300

    Spying is now more tamper-proof
    I've also added documentation to 'spy' and 'nospy'.

--- src/etc-bots/spy.lisp
+++ src/etc-bots/spy.lisp
@@ -8,14 +8,20 @@
 	  (setf (channels-to-spy bot) nil))
 
 (defcommand spy ("on (\\S+)" channel)
+  "Syntax: 'spy on <channel>' - Echoes all messages in <channel> to the current ~
+   channel. The bot can only spy on channels which it has already joined."
   (with-accessors ((channels channels-to-spy)) *bot*
-    (if (find *channel* (alref channel channels) :test #'equalp)
+    (if (or (find *channel* (alref channel channels) :test #'equalp)
+            (string-equal channel *channel*)
+            (not (find channel (channels *bot*) :test #'equalp)))
         "Have you nothing better to do than torture a bot?"
         (progn
           (push *channel* (alref channel channels))
           "I'll be sneaky"))))
 
 (defcommand nospy ("(\\S+)" channel)
+  "Syntax: 'nospy <channel>' - Stops echoing messages from <channel> to the ~
+   current channel."
   (with-accessors ((channels channels-to-spy)) *bot*
     (setf (alref channel channels)
           (remove *channel* (alref channel channels) :test #'equalp)))
